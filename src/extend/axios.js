b18755045ca232acea03aaa7c0961ec99051c6eb
郭风武 <guofengwu@wecash.net>
2017-11-01 14:20:48 +0800

    zxc

--- src/extend/axios.js
+++ src/extend/axios.js
@@ -6,35 +6,35 @@ import axios from 'axios';
  */
 export default {
     install({ prototype: pro }) {
-        pro.$fetch = fac.bind(null, pro);
+        pro.$fetch = fac.bind(pro);
     }
 };
 
 
 // 工厂模式
-function fac(pro, opt) {
-    pro.$Loading.start() || axios.create({ timeout: 6e5 })(opt).then(({ data }) => suc(pro, data, opt.suc)).catch(({ message }) => err(pro, message, opt.err));
+function fac(opt) {
+    this.$Loading.start() || axios.create({ timeout: 6e5 })(opt).then(({ data }) => suc.call(this, data, opt.suc)).catch(({ message }) => err.call(this, message, opt.err));
 }
 
 
 // 成功回调
-function suc(pro, res, cb = function () { }) {
+function suc(res, cb = function () { }) {
     if (Object.prototype.toString.call(res).slice(8, -1).toLowerCase() !== 'object') return;
     if (Number.isInteger(res.code)) {
         if (res.code === 0) {
-            pro.$Loading.finish() || cb(res);
+            this.$Loading.finish() || cb(res);
         }
         else {
-            pro.$Loading.finish() || pro.$Notice.warning({ title: '状态异常', desc: 'Code 不等于 0' });
+            this.$Loading.finish() || this.$Notice.warning({ title: '状态异常', desc: 'Code no equal 0' });
         }
     }
     else {
-        pro.$Loading.error() || pro.$Notice.error({ title: '状态异常', desc: 'Code 数据类型错误' });
+        this.$Loading.error() || this.$Notice.error({ title: '状态异常', desc: 'Code Type Error' });
     }
 }
 
 
 // 失败回调
-function err(pro, msg, cb = function () { }) {
-    pro.$Loading.error() || cb() || pro.$Notice.error({ title: '接口异常', desc: msg });
+function err(msg, cb = function () { }) {
+    this.$Loading.error() || cb() || this.$Notice.error({ title: '接口异常', desc: msg });
 }
